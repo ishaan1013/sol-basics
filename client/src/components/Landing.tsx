71bb3070b21da7f4c56ceb5d064c1faa8ddc88ae
ishaan1013 <ishaan1013@gmail.com>
2022-06-27 13:30:16 -0400

    landing

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,6 +1,14 @@
+import Eth1 from '../assets/eth1.png'
+
 const Landing = () => {
     return (
-        <h1>Landing</h1>
+        <section className="w-full h-screen flex justify-center pb-8">
+            <div className="container h-full flex justify-between items-center relative">
+                <img src={Eth1} alt="Background Image" className="absolute right-0 mt-36" />
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-blue-300 font-extrabold text-[6rem]">Exchange.</h1>
+                {/* <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-blue-600 font-extrabold text-[6rem]">Exchange.</h1> */}
+            </div>
+        </section>
     )
 }
 
