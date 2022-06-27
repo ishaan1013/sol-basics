94c0ec42ba3095d5d54371ed84c13eb72150f5bf
ishaan1013 <ishaan1013@gmail.com>
2022-06-27 12:52:35 -0400

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 
 import { Landing, Loader, Navbar, Services, Transactions } from './components';
 
@@ -7,15 +6,20 @@ const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen">
-        <div>
-          <Navbar />
-          <Landing />
-        </div>
-
-        <Services />
-        <Transactions />
-    </div>
+    <>
+      <div className="bg-yellow-300 -z-10 w-12 h-96 rounded-[100%] absolute lg:right-[32vw] md:right-[48vw] right-[60vw] -top-28 rotate-[20deg] blur-[80px] sm:opacity-30 opacity-10"/>
+      <div className="bg-pink-300 -z-10 w-12 h-96 rounded-[100%] absolute lg:right-[24vw] md:right-[32vw] right-[48vw] -top-48 rotate-[20deg] blur-3xl sm:opacity-50 opacity-10"/>
+      <div className="bg-blue-300 -z-10 w-12 h-96 rounded-[100%] absolute lg:right-[18vw] md:right-[24vw] right-[24vw] -top-4 rotate-[20deg] blur-[80px] sm:opacity-20 opacity-10"/>
+      <div className="bg-green-300 -z-10 w-12 h-96 rounded-[100%] absolute lg:right-[6vw] md:right-[6vw] right-[6vw] -top-48 rotate-[20deg] blur-3xl sm:opacity-30 opacity-10"/>
+      <div className="min-h-screen">
+          <div>
+            <Navbar />
+            <Landing />
+          </div>
+          <Services />
+          <Transactions />
+      </div>
+    </>
   )
 }
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,42 @@
+import { useState } from 'react';
+import Logo from '../assets/logo.png';
+import { MdOutlineMenu, MdClose } from 'react-icons/md'; 
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <h1>Navbar</h1>
+        <nav className="px-2 sm:px-4 pt-5 bg-transparent">
+            <div className="container flex flex-wrap justify-between items-center mx-auto border-b-white/20 border-b-[1px] pb-4">
+                <img src={Logo} className="mr-3 h-12 pointer-events-none select-none" alt="Logo" />
+
+                <button 
+                // onClick={() => {setMenuOpen(!menuOpen)}}
+                type="button" 
+                className="z-20 inline-flex items-center ml-3 p-2 rounded-lg text-sm md:hidden focus:outline-none focus:ring-2 text-white hover:hover:bg-blue-50/10 focus:ring-gray-600" 
+                aria-controls="mobile-menu"
+                onClick={() => {setMenuOpen(!menuOpen)}}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    {!menuOpen ? <MdOutlineMenu size="1.75em" /> : <MdClose size="1.75em" />}
+                </button>
+
+                <div className="hidden w-full md:block md:w-auto">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <p className="cursor-pointer block py-2 pr-4 pl-3 text-white bg-blue-400 rounded-xl md:bg-transparent md:hover:bg-blue-50/10 hover:bg-blue-400 md:px-3 md:py-2 text-[1rem]" aria-current="page">Exchange</p>
+                        </li>
+                        <li>
+                            <p className="cursor-pointer block py-2 pr-4 pl-3 text-white bg-blue-400 rounded-xl md:bg-transparent md:hover:bg-blue-50/10 hover:bg-blue-400 md:px-3 md:py-2 text-[1rem]" aria-current="page">History</p>
+                        </li>
+                    </ul>
+                </div>
+                {menuOpen &&
+                <div className="md:hidden absolute top-0 right-0 h-screen bg-slate-900/50 backdrop-blur-lg sm:w-2/5 xs:w-3/5 w-4/5">
+                    
+                </div>
+                }
+            </div>
+        </nav>
     )
 }
 
