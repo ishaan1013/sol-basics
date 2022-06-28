d974c8336636a34079b2892644cb771ec52d34e7
ishaan1013 <ishaan1013@gmail.com>
2022-06-27 21:08:46 -0400

    card + landing styles

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,15 +1,31 @@
 import Eth1 from '../assets/eth1.png'
+import Eth2 from '../assets/eth2.png'
 
 const Landing = () => {
     return (
-        <section className="w-full h-screen flex justify-center pb-8">
+        <section className="w-full h-screen flex justify-center pt-8">
             <div className="container h-full flex justify-between items-center relative">
                 <div className="bg-violet-500 -z-10 w-12 h-96 rounded-[100%] absolute left-36 -mt-24 -rotate-12 blur-[80px] opacity-60"/>
                 <div className="bg-blue-500 -z-10 w-12 h-96 rounded-[100%] absolute left-80 mt-16 -rotate-[70deg] blur-[80px] opacity-60"/>
-                <img src={Eth1} alt="Background Image" className="absolute right-0 mt-36" />
+                <img src={Eth1} alt="Background Image" className="absolute -right-44 -z-10 mb-20" />
                 <div>
-                    <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-blue-300 font-extrabold text-[6rem]">Exchange.</h1>
-                    <button className="bg-gradient-to-r from-violet-400 hover:from-violet-500 to-blue-300 hover:to-blue-400 w-full p-2 mt-2 rounded-lg text-[#0f121a] font-bold text-lg">Connect Wallet</button>
+                    <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-blue-300 font-extrabold text-[6rem]">Exchange</h1>
+                    <button className="select-none bg-gradient-to-r from-violet-400 hover:from-violet-500 to-blue-300 hover:to-blue-400 w-full p-2 mt-2 rounded-lg text-[#0f121a] font-bold text-lg">Connect Wallet</button>
+                </div>
+
+                <div>
+                    <div className="select-none overflow-hidden relative p-6 w-[26rem] h-52 mb-8 bg-gradient-to-bl from-white/[0.05] to-white/[0.01] border-[1px] border-white/10 rounded-3xl backdrop-blur-xl -z-20">
+                        <div className="bg-violet-500 -z-10 w-24 h-96 rounded-[100%] absolute right-16 -mt-36 blur-2xl -rotate-12 opacity-50"/>
+                        <div className="bg-blue-500 -z-10 w-24 h-96 rounded-[100%] absolute right-36 mt-4 blur-2xl -rotate-12 opacity-50"/>
+                        <div className="absolute w-full left-0 bottom-0 bg-white/10 h-16"/>
+                        <img src={Eth2} alt="Eth Image" className="absolute bottom-0 left-0 mb-3 ml-6 w-10 h-10 opacity-90" />
+
+                        <p className="text-white/60 italic font-light">Address</p>
+                        <h1 className="text-white/90 text-xl font-bold">...</h1>
+                    </div>
+                    <div className="w-[26rem] h-96 rounded-3xl bg-gradient-to-bl from-white/[0.05] to-white/[0.01] border-[1px] border-white/50 backdrop-blur-3xl">
+
+                    </div>
                 </div>
 
             </div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/eth1.png';
 import { MdOutlineMenu, MdClose } from 'react-icons/md'; 
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <nav className="absolute w-full px-2 sm:px-4 pt-5 bg-transparent">
+        <nav className="absolute w-full px-2 sm:px-4 pt-5 bg-transparent select-none">
             <div className="container flex flex-wrap justify-between items-center mx-auto border-b-white/[0.05] border-b-[1px] pb-4">
                 <img src={Logo} className="mr-3 h-12 pointer-events-none select-none" alt="Logo" />
 
@@ -20,13 +20,13 @@ const Navbar = () => {
                     {!menuOpen ? <MdOutlineMenu size="1.75em" /> : <MdClose size="1.75em" />}
                 </button>
 
-                <div className="hidden w-full md:block md:w-auto">
+                <div className="hidden w-full md:block md:w-auto z-50">
                     <ul className="flex flex-col mt-4 md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium">
                         <li>
                             <p className="cursor-pointer block py-2 pr-4 pl-3 text-white rounded-xl md:bg-transparent md:hover:bg-blue-50/10 hover:bg-blue-400 md:px-3 md:py-2 text-[1rem]" aria-current="page">Exchange</p>
                         </li>
                         <li>
-                            <p className="cursor-pointer block py-2 pr-4 pl-3 text-white rounded-xl md:bg-transparent md:hover:bg-blue-50/10 hover:bg-blue-400 md:px-3 md:py-2 text-[1rem]" aria-current="page">History</p>
+                            <p className="cursor-pointer block py-2 pr-4 pl-3 text-white rounded-xl md:bg-transparent md:hover:bg-blue-50/10 hover:bg-blue-400 md:px-3 md:py-2 text-[1rem]" aria-current="page">Transactions</p>
                         </li>
                     </ul>
                 </div>
@@ -37,7 +37,7 @@ const Navbar = () => {
                             <p className="slide cursor-pointer block my-2 py-2 pr-4 pl-3 text-white rounded-lg bg-transparent hover:bg-blue-50/10 text-[1rem]" aria-current="page">Exchange</p>
                         </li>
                         <li>
-                            <p className="slide cursor-pointer block py-2 pr-4 pl-3 text-white rounded-lg bg-transparent hover:bg-blue-50/10 text-[1rem]" aria-current="page">History</p>
+                            <p className="slide cursor-pointer block py-2 pr-4 pl-3 text-white rounded-lg bg-transparent hover:bg-blue-50/10 text-[1rem]" aria-current="page">Transactions</p>
                         </li>
                     </ul>
                 </div>
