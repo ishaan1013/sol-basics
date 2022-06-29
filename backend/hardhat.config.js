dd953040d813a4195a3ea1ed887069db3ca0bf7a
ishaan1013 <ishaan1013@gmail.com>
2022-06-29 17:00:26 -0400

    finish contract

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,21 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/a-rpDeR9TY12Ugo_bTXdK_8c-SP2TM-4",
+      accounts: ["2200b89f5ba00a9b97f7f28502af4636a367af8b52384a8bfad9c2ebbbafdfa5"]
+    }
+  }
+}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json"
+
+export const contractABI = abi.abi
+export const contractAddress = "0xBCa2Fa9e252e5cE2DE7518936d5261403f407046"
